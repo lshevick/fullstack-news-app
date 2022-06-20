c5523ba50e9bd5010bb142c73ef5edffa316f78d
Levi Shevick <levidshevick@gmail.com>
2022-06-20 14:26:02 -0400

    inital file setup

---
+++ accounts/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    pass
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True)
+    avatar = models.ImageField(upload_to='profiles/')
+    display_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.user.username
